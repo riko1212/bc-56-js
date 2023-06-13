b1f51a59a95f3d4e2440516910a053b32a989a5d
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-06-13 07:33:31 +0300

    +++

--- js/examples.js
+++ js/examples.js
@@ -1,52 +1,160 @@
 /*
-? Напиши скрипт, який для об'єкта user послідовно:
-? додає властивість mood зі значенням 'happy'
-? замінює значення hobby на 'skydiving'
-? замінює значення premium на false
-? виводить вміст об'єкта user у форматі ключ: значення використовуючи Object.keys() і for...of
+? Перепиши функцію так, щоб вона приймала один об'єкт параметрів,
+? замість набору незалежних аргументів.
 */
-const user = {
-  name: 'Mango',
-  age: 20,
-  hobby: 'html',
-  premium: true,
-};
+// function calcBMI( weight, height ) {
+//   const numericWeight = Number(weight.replace(',', '.'));
+//   const numericHeight = Number(height.replace(',', '.'));
 
-user.mood = 'happy';
-user.hobby += ' skydiving';
-// user.hobby = ['html', 'skydiving'];
-user.premium = false;
+//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
+// }
 
-console.log(user);
+// Було
+// console.log(calcBMI('88,3', '1.75'));
+// console.log(calcBMI('68,3', '1.65'));
+// console.log(calcBMI('118,3', '1.95'));
 
-// const userKeys = Object.keys(user);
-
-for (const key of Object.keys(user)) {
-  console.log(`${key} : ${user[key]}`);
-}
+// Очікується
+// console.log(
+//   calcBMI({
+//     weight: '88,3',
+//     height: '1.75',
+//   })
+// );
+// console.log(
+//   calcBMI({
+//     weight: '68,3',
+//     height: '1.65',
+//   })
+// );
+// console.log(
+//   calcBMI({
+//     weight: '118,3',
+//     height: '1.95',
+//   })
+// );
 
 /*
-? У нас є об'єкт, де зберігаються зарплати нашої команди.
-? Напишіть код для підсумовування всіх зарплат та збережіть результат у змінній total.
-? Повинно вийти 390. Якщо об'єкт salaries порожній, результат має бути 0.
+? Перепиши функцію так, щоб вона приймала один об'єкт параметрів,
+? замість набору незалежних аргументів.
 */
-// const salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130,
+// function printContactsInfo( names, phones ) {
+//   const nameList = names.split(',');
+//   const phoneList = phones.split(',');
+
+//   for (let i = 0; i < nameList.length; i += 1) {
+//     console.log(`${nameList[i]}: ${phoneList[i]}`);
+//   }
+// }
+
+// Було
+// printContactsInfo(
+//   'Jacob,William,Solomon,Artemis',
+//   '89001234567,89001112233,890055566377,890055566300'
+// );
+
+// Очікується
+// printContactsInfo({
+//   names: 'Jacob,William,Solomon,Artemis',
+//   phones: '89001234567,89001112233,890055566377,890055566300',
+// });
+
+/*
+? Перепиши функцію так, щоб вона приймала один об'єкт параметрів,
+? замість набору незалежних аргументів.
+*/
+// function getBotReport(companyName, repairBots, defenceBots) {
+
+//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// }
+
+// Було
+// console.log(getBotReport('Cyberdyne Systems', 150, 50));
+
+// Очікується
+// console.log(
+//   getBotReport({
+//     companyName: 'Cyberdyne Systems',
+//     bots: {
+//       repair: 150,
+//       defence: 50,
+//     },
+//   })
+// ); // "Cyberdyne Systems has 200 bots in stock"
+
+/*
+? Напиши функцію getStockReport(companyName, stock), щоб вона приймала об'єкт параметрів із властивостями companyName і stock і виводила репорт
+? про кількість товарів на складі будь-якої компанії.
+*/
+
+// function getStockReport(obj) {
+// }
+
+// console.log(
+//   getStockReport({
+//     companyName: 'Cyberdyne Systems',
+//     stock: {
+//       repairBots: 150,
+//       defenceBots: 50,
+//     },
+//   })
+// ); //"Cyberdyne Systems has 200 items in stock"
+
+// console.log(
+//   getStockReport({
+//     companyName: 'Belacci',
+//     stock: {
+//       shoes: 20,
+//       skirts: 10,
+//       hats: 5,
+//     },
+//   })
+// ); // "Belacci has 35 item in stock"
+
+/*
+? Напиши функцію createContact(partialContact) так,
+? щоб вона повертала новий об'єкт контакту з доданими властивостями id і createdAt,
+? а також list зі значенням "default" якщо у partialContact немає такої властивості.
+*/
+// const generateId = function () {
+//   return '_' + Math.random().toString(36).substr(2, 9);
 // };
 
-/*
-? Напишіть ф-цію calcTotalPrice(stones, stoneName), яка приймає масив об'єктів та рядок з назвою каменю.
-? Ф-ція рахує і повертає загальну вартість каміння з таким ім'ям.
-*/
-// const stones = [
-//   { name: 'Смарагд', price: 1300, quantity: 4 },
-//   { name: 'Діамант', price: 2700, quantity: 3 },
-//   { name: 'Сапфір', price: 400, quantity: 7 },
-//   { name: 'Щебінь', price: 200, quantity: 2 },
-// ];
+// console.log(
+//   createContact({
+//     name: 'Mango',
+//     email: 'mango@mail.com',
+//     list: 'friends',
+//   })
+// );
 
-// console.log(calcTotalPrice(stones, 'Смарагд')); // 5200
-// console.log(calcTotalPrice(stones, 'Діамант')); // 8100
-// console.log(calcTotalPrice(stones, 'Аконіт')); // Каменя за такою назвою не знайдено
+// console.log(
+//   createContact({
+//     name: 'Poly',
+//     email: 'poly@hotmail.com',
+//   })
+// );
+
+/*
+? Напиши функцію transformUsername(user) так, щоб вона повертала новий об'єкт із властивістю fullName, замість firstName та lastName.
+*/
+
+// console.log(
+//   transformUsername({
+//     id: 1,
+//     firstName: 'Jacob',
+//     lastName: 'Mercer',
+//     email: 'j.mercer@mail.com',
+//     friendCount: 40,
+//   })
+// );
+
+// console.log(
+//   transformUsername({
+//     id: 2,
+//     firstName: 'Adrian',
+//     lastName: 'Cross',
+//     email: 'a.cross@hotmail.com',
+//     friendCount: 20,
+//   })
+// );
--- js/main.js
+++ js/main.js
@@ -1,95 +1,31 @@
 /*
- * Створення об'єкта (конструктор, литерал).
- * Властивості, ключі (ім'я) та значення
+ * Операція spread замість concat і slice
  */
 
+//* Копія масива
+
+//* Об'єднання масивів
+
+//* Розпилення масиву у функцію
+
+/*
+ * Операція spread замість Object.assign({}, obj)
+ */
 // const user = {
-//   name: 'Ted',
-//   age: 40,
-//   profession: {
-//     position: 'Manager',
-//     expirience: 2,
-//   },
+//   firstName: 'Jonathan',
+//   lastName: 'Barnett',
+//   age: 30,
 // };
 
-// // console.log(user.age);
-
-// const obj = Object.create(user);
-
-// obj.age = 50;
-// // // obj.name = 'Bill';
-// console.log(obj);
-
-/*
- * Доступ до властивостей(obj.key, obj[key]).
- * Додавання та зміна властивостей
- */
-
-// user.adult = true;
-
-// console.log(user);
-
-// user.length
-
-/*
- * Короткий запис властивостей
- */
-
-/* <input type="text" name="username"> */
-/* <input type="email" name="useremail"> */
-
-// const userName = 'Bob';
-// const userEmail = 'test@mail.com';
-
+//* Розпилення складних типів
 // const user = {
-//   userName,
-//   userEmail,
+//   firstName: 'Jonathan',
+//   lastName: 'Barnett',
+//   age: 30,
+//   someArr: [1, 2, 3, 4],
 // };
-
-// console.log(user);
 
 /*
- * Присвоєння за посиланням
+ * Операція rest
  */
-
-// const user = {
-//   firstName: 'Brett',
-//   lastName: 'Romero',
-//   age: 20,
-// };
-
-// const user2 = user;
-
-// console.log(user);
-// console.log(user2);
-
-// user2.age = 40;
-
-// console.log(user);
-// console.log(user2);
-
-/*
- * Все у цьому світі об'єкти
- */
-
-// const arr = [1, 2, 3];
-
-// const obj = {
-//   0: 1,
-//   1: 2,
-//   2: 3,
-//   length: 3,
-// };
-
-// arr['3'] = 4;
-
-// console.log(arr);
-// console.log(obj);
-
-// let fn = function () {
-//   console.log('Hello');
-// };
-
-// fn = 123;
-
-// console.dir(fn);
+//? Напишіть функцію sum, яка складає довільну кількість аргументів

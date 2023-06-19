8f41f9aee7b1d3f10726fa4f8d38066285f2c4db
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-06-19 09:24:53 +0300

    +++

---
+++ js/main.js
@@ -0,0 +1,189 @@
+/*
+ *  Контекст виконання this
+ */
+
+/*
+ * this існує тільки всередині функцій.
+ * На this не впливає те де функція була //! ОГОЛОШЕНА.
+ * На this впливає те де функція //! ВИКЛИКАЄТЬСЯ.
+ */
+
+//TODO: Розглянемо як this поводиться в методах
+// const user = {
+//   name: 'Luis',
+//   age: 30,
+
+//   showThis() {
+//     console.log('This --->', this);
+//   },
+
+//   showName() {
+//     console.log(this.name);
+//   },
+// };
+
+//TODO: Розглянемо як this поводиться у звичайних функціях (суворий, не суворий режим)
+//? Function expression
+// const greet = function () {
+//   console.log('This --->', this);
+//   console.log('Hello');
+// };
+
+// greet();
+
+//? Function declaration
+// function greet() {
+//   console.log('This --->', this);
+//   console.log('Hello');
+// }
+
+// greet();
+
+//? Arrow function
+// const greet = () => {
+//   console.log('This --->', this);
+//   console.log('Hello');
+// };
+
+// greet();
+
+//TODO: Присвоєння функції як методу об'єкта
+// const showThis = function () {
+//   console.log('This --->', this);
+// };
+
+// const showName = function () {
+//   console.log(`Hello ${this.name}`);
+// };
+
+// const user = {
+//   name: 'Luis',
+//   age: 30,
+// };
+
+//TODO: Виклик методу об'єкта без контексту
+// const user = {
+//   name: 'Luis',
+//   age: 30,
+
+//   showUserThis() {
+//     console.log('This --->', this);
+//   },
+
+//   showUserName() {
+//     console.log(this.name);
+//   },
+// };
+
+//TODO: This в callback функціях
+// const user = {
+//   name: 'Luis',
+//   age: 30,
+
+//   showUserThis() {
+//     console.log('This --->', this);
+//   },
+
+//   showUserName() {
+//     console.log(this.name);
+//   },
+// };
+
+// const someFunction = function (callback) {
+
+//   callback();
+// };
+
+// someFunction(user.showUserThis);
+
+//TODO: This у стрілочних функціях. Стрілочні функції не мають свого this, this в стрілках завжди посилається на батьківський this.
+// const user = {
+//   name: 'Luis',
+//   age: 30,
+//   changeAge: (newAge) => {
+//     console.log(`this ---->`, this);
+//     this.age = newAge;
+//   },
+// };
+
+// user.changeAge(40);
+
+/*
+? Яким буде результат виконання цього коду?
+*/
+// let user = {
+//   name: 'Джон',
+
+//   go() {
+//     console.log(this);
+//     console.log(this.name);
+//   },
+// };
+
+// const goFn = user.go;
+
+// goFn();
+// user.go();
+
+/*
+? Тут функція makeUser повертає об'єкт.
+? Яким буде результат при зверненні до об'єкта ref? Чому?
+*/
+// const makeUser = function () {
+//   return {
+//     name: 'Джон',
+//     ref: this,
+//   };
+// };
+
+// const user = makeUser();
+
+// console.log(user.ref.name);
+
+/*
+? Яким буде результат console.log
+*/
+// function makeUser() {
+
+//   return {
+//     name: 'Джон',
+//     ref() {
+
+//       return this;
+//     },
+//   };
+// }
+
+// const user = makeUser();
+
+// console.log(user);
+// console.log(user.ref().name);
+
+/*
+? Це ladder (сходи) – об'єкт, який дозволяє підніматися вгору та спускатися:
+*/
+// const ladder = {
+//   step: 0,
+
+//   up() {
+//     this.step += 1;
+//   },
+
+//   down() {
+//     this.step -= 1;
+//   },
+
+//   showStep() {
+//     console.log(this.step);
+//   },
+// };
+
+// Тепер, якщо нам потрібно зробити кілька послідовних викликів, ми можемо зробити це так:
+
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep();
+
+// Змініть код методів up, down та showStep таким чином, щоб їх виклик можна було зробити по ланцюжку, наприклад:
+// ladder.up().up().down().showStep();

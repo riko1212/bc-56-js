73c78cf365c0add170091d92edad8ac644ac5876
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-06-20 09:27:14 +0300

    +++

---
+++ js/prototypes.js
@@ -0,0 +1,15 @@
+/*
+ * Ланцюжки прототипів.
+ * Object.create().
+ * obj.hasOwnProperty()
+ */
+const user = {
+  firstName: 'Oleksii',
+  age: 40,
+};
+
+// https://learn.javascript.ru/article/native-prototypes/native-prototypes-classes.svg
+
+//* Власні та не власні властивості об'єкта
+
+//* Перевірка власних властивостей obj.hasOwnProperty()

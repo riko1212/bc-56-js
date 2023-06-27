ff7a7934a6b343f94d98b41ce180890aee165ec6
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-06-27 08:17:46 +0300

    +++

---
+++ js/find-country.js
@@ -0,0 +1,43 @@
+const countries = [
+  {
+    name: 'Ukraine',
+    capital: 'Kyiv',
+    population: '41 342 465',
+    area: '603.628',
+  },
+  {
+    name: 'Spain',
+    capital: 'Madrid',
+    population: '47 450 795',
+    area: '505.990',
+  },
+  {
+    name: 'Philippines',
+    capital: 'Manila',
+    population: '109 035 343',
+    area: '300.000',
+  },
+  {
+    name: 'Angola',
+    capital: 'Luanda',
+    population: '31 127 674',
+    area: '1.246.700',
+  },
+  {
+    name: 'Gibraltar',
+    capital: 'Gibraltar',
+    population: '34 003',
+    area: '6.8',
+  },
+];
+
+const searchInputEl = document.querySelector('.js-search-input');
+const outputError = document.querySelector('.js-output-error');
+const countryCardEl = document.querySelector('.js-country-card');
+
+const createCountryCard = ({ name, capital, population, area } = {}) => {
+  return `<li class="country-card__item"><strong>Країна:</strong> ${name}</li>
+     <li class="country-card__item"><strong>Столиця:</strong> ${capital}</li>
+     <li class="country-card__item"><strong>Населення:</strong> ${population}</li>
+     <li class="country-card__item"><strong>Площа:</strong> ${area}км<sup>2</sup></li>`;
+};
---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+const randomColor = () =>
+  '#' + (Math.random().toString(16) + '000000').substring(2, 8).toUpperCase();
+
+const paintingSectionsInRandomColor = () => {
+  document.querySelectorAll('.section').forEach((section) => {
+    section.style.backgroundColor = randomColor();
+  });
+};

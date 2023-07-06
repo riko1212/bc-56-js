5c6fcc14ed0886cc68742a9d2042048bd4216423
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-07-06 13:04:13 +0300

    +++

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,41 +1,58 @@
 // https://openweathermap.org/current
+import makeWeatherCard from '../templates/weather-card.hbs'
+import { fetchWeather } from './weather-api'
+
+// fetch('https://api.thecatapi.com/v1/search?breed_ids=beng', {
+//   headers: {
+//     'x-api-key':'live_Y1L53BkbeQ0rKz6SFWIVspjzTlD5d5nxUI61VfjsZLzbHGjAIfn0nQLtY4HSLTyD'
+//   }
+// }).then(response => {
+//   console.log(response);
+//   if (!response.ok) {
+//     throw new Error(response.status)
+//   }
+//   return response.json()
+// }).then(data => {
+//   console.log(data);
+// }).catch(err => {
+//   console.log(err);
+// })
 
 const weatherFormEl = document.querySelector('.js-search-form');
 const weatherWrapperEl = document.querySelector('.js-weather__wrapper');
 
-// const convertSecondsToHoursAndMinutes = seconds => {
-//   const date = new Date(seconds * 1000);
+//live_Y1L53BkbeQ0rKz6SFWIVspjzTlD5d5nxUI61VfjsZLzbHGjAIfn0nQLtY4HSLTyD
 
-//   return `${new String(date.getHours()).padStart(2, 0)}:${new String(date.getMinutes()).padStart(
-//     2,
-//     0
-//   )}`;
-// };
+const convertSecondsToHoursAndMinutes = seconds => {
+  const date = new Date(seconds * 1000);
+  // return `${date.getHours()}:${date.getMinutes()}`
 
-const onWeatherFormSubmit = event => {
-  event.preventDefault();
-
-  const weatherQuery = event.currentTarget.elements.user_country.value;
-
-  fetchCurrentWeather(weatherQuery)
-    .then(data => {
-      console.log(data);
-
-      data.sys.sunrise = convertSecondsToHoursAndMinutes(data.sys.sunrise);
-      data.sys.sunset = convertSecondsToHoursAndMinutes(data.sys.sunset);
-
-      weatherWrapperEl.innerHTML = createWeatherCard(data);
-    })
-    .catch(err => {
-      if (err.message === '404') {
-        alert('Такого міста не знайдено');
-
-        weatherWrapperEl.innerHTML = '';
-      }
-    })
-    .finally(() => {
-      event.target.reset();
-    });
+  return `${new String(date.getHours()).padStart(2, 0)}:${new String(date.getMinutes()).padStart(
+    2,
+    0
+  )}`;
 };
 
-weatherFormEl.addEventListener('submit', onWeatherFormSubmit);
+
+const onFormSubmit = (event) => {
+  event.preventDefault()
+  const weatherQuery = event.currentTarget.elements.user_city.value;
+  fetchWeather(weatherQuery).then(data => {
+    data.sys.sunrise = convertSecondsToHoursAndMinutes(data.sys.sunrise)
+    data.sys.sunset = convertSecondsToHoursAndMinutes(data.sys.sunset)
+    const cardMarkup = makeWeatherCard(data)
+    weatherWrapperEl.innerHTML = cardMarkup
+  }).catch(err => {
+    console.log(err);
+    weatherWrapperEl.innerHTML = ''
+  }).finally(() => {
+  event.target.reset()
+})
+}
+
+
+weatherFormEl.addEventListener('submit', onFormSubmit)
+
+
+
+
